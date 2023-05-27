475d920ddbec5eff171d40d1f8e22b795208488d
IvanAstr <IvanAst381@gmail.com>
2023-05-27 11:48:02 +0700

    fix2

--- frontend/src/pages/AdsMore.jsx
+++ frontend/src/pages/AdsMore.jsx
@@ -8,11 +8,16 @@ import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts';
 import AdsList from '../components/AdsList';
 
 const AdsMore = ()=>{
-    return(
-        <Container>
-            <Row className='mt-4'>
-                more
-            </Row>
+    return (
+        <Container
+            className="d-flex justify-content-center align-items-center"
+            style={{ height: window.innerHeight - 54 }}
+        >
+            <Card style={{ width: 600 }} className="p-5">
+                <Form className='d-flex flex-column' lg='2'>
+
+                </Form>
+            </Card>
         </Container>
     )
 }
